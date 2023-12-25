c4b3df5567fe69b21234e26cd63a57ea963d9a4c
Artem Levchenko <evgn.main@gmail.com>
2023-12-25 15:50:41 +0200

    Airplane class

--- Sana05/Sana05/Program.cs
+++ Sana05/Sana05/Program.cs
@@ -2,6 +2,74 @@
 
 var startDate = new MyDate(2023, 12, 21, 12, 00);
 var finishDate = new MyDate(2023, 12, 21, 22, 00);
+var airplane = new Airplane("Ukraine", "Egypt", startDate, finishDate);
+
+Console.WriteLine("Total time in minutes:  {0}", airplane.GetTotalTime());
+Console.WriteLine("Is arriving today: {0}", airplane.IsArrivingToday());
+
+class Airplane
+{
+    protected string _startCity { get; set; } = null!;
+    protected string _finishCity { get; set; } = null!;
+    protected MyDate _startDate { get; set; } = null!;
+    protected MyDate _finishDate { get; set; } = null!;
+
+    public Airplane()
+    {
+        
+    }
+
+    public Airplane(string startCity, MyDate startDate)
+    {
+        _startCity = startCity;
+        _startDate = startDate;
+    }
+    public Airplane(string startCity, string finishCity, MyDate startDate, MyDate finishDate)
+    {
+        _startCity = startCity;
+        _finishCity = finishCity;
+        _startDate = startDate;
+        _finishDate = finishDate;
+    }
+
+    public Airplane(Airplane airplane)
+    {
+        _startCity = airplane._startCity;
+        _finishCity = airplane._finishCity;
+        _startDate = airplane._startDate;
+        _finishDate = airplane._finishDate;
+    }
+    public int GetTotalTime()
+    {
+        return (_finishDate.Hours - _startDate.Hours)*60 + (_finishDate.Minutes - _startDate.Minutes);
+    }
+
+    public bool IsArrivingToday()
+    {
+        return _finishDate.Day == _startDate.Day;
+    }
+
+    public string StartCity
+    {
+        get => _startCity;
+        set => _startCity = value;
+    }
+    public string FinishCity
+    {
+        get => _finishCity;
+        set => _finishCity = value;
+    }
+    public MyDate StartDate
+    {
+        get => _startDate;
+        set => _startDate = value;
+    }
+    public MyDate FinishDate
+    {
+        get => _finishDate;
+        set => _finishDate = value;
+    }
+}
 
 class MyDate
 {
